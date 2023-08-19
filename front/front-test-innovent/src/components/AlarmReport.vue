d9bea1d519d36bd7f9c5b3d9fe561f92216ce24a
Laurent Betti <laurent.betti@lumapps.com>
2023-08-19 19:03:49 +0200

    clean front app and set up basis for alarm report

---
+++ front/front-test-innovent/src/components/AlarmReport.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        { id: 1, name: 'Pouet' },
+        { id: 2, name: 'Plouf' }
+      ]
+    }
+  }
+}
+</script>
+
+<template>
+  <main><h1>This is the Alarm Report</h1></main>
+  <li v-for="i in items" :key="i.id">
+    <a>Name: {{ i.name }}</a>
+  </li>
+</template>
