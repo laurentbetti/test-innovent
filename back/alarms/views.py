2c24aeba19d723da5cddafb3de5cc1685dd57f80
Laurent Betti <laurent.betti@lumapps.com>
2023-08-25 15:06:14 +0200

    finalize simple version of "report" view

--- back/alarms/views.py
+++ back/alarms/views.py
@@ -1,14 +1,45 @@
 from django.db.models import Count
 from django.http import JsonResponse
-from alarms.models import Alarm
+from alarms.models import Producer
 
 
 def report(request):
-    data = list(
-        Alarm.objects.values("producer_id", "alarm_code")
-        .annotate(alarm_count=Count("time"))
-        .order_by("producer_id", "-alarm_count")
+    producer_alarms = list(
+        Producer.objects.select_related("alarm__alarm_code")
+        .values(
+            "id", "display_name", "alarm__alarm_code__id", "alarm__alarm_code__name"
+        )
+        .annotate(trigger_count=Count("alarm__alarm_code__id"))
+        .order_by("id", "-trigger_count")
+    )
+
+    producer_top_two_alarms = []
+    nb_alarms = 0
+    prev_producer_id = None
+    for pa in producer_alarms:
+        if pa["id"] == prev_producer_id:
+            if nb_alarms < 2:
+                producer_top_two_alarms.append(pa)
+                nb_alarms += 1
+        else:
+            producer_top_two_alarms.append(pa)
+            prev_producer_id = pa["id"]
+            nb_alarms = 1
+
+    formatted_producer_alarms = list(
+        map(
+            lambda pa: (
+                {
+                    "id": pa["id"],
+                    "name": pa["display_name"],
+                    "alarmCodeId": pa["alarm__alarm_code__id"],
+                    "alarmCodeName": pa["alarm__alarm_code__name"],
+                    "alarmCount": pa["trigger_count"],
+                }
+            ),
+            producer_top_two_alarms,
+        )
     )
 
     # TODO: see if there is a better alternative that safe=False
-    return JsonResponse(data, safe=False)
+    return JsonResponse(formatted_producer_alarms, safe=False)
